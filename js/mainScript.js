ccfbc845f9ac9b969bb605daec1a88236cc20291
thisSasha <trhfjfn@gmail.com>
2023-01-31 20:24:54 +0300

    v 1.0

---
+++ js/mainScript.js
@@ -0,0 +1,19 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.onclick = function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = 0;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    };
+});
